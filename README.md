02d379589a9d549168c0379c211610943bd2e3af
bcostaaa01 <bcostinha01@gmail.com>
2022-08-04 01:17:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-rimdrg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-rimdrg)
---
+++ src/components/ApiData.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+<button @click="logData">Display data</button>
+{{result}}
+</div>
+</template>
+
+<script>
+import {ref} from 'vue'
+
+export default {
+  setup () {
+    const result = ref(null)
+
+    fetch("https://jsonplaceholder.typicode.com/todos")
+    .then(response => response.json())
+    .then(data => result.value = data)
+
+    return {result}
+  }
+}
+
+
+
+</script>
